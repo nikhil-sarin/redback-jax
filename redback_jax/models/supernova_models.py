1dd50696ffc72151aa8a13ba5f120e16d29ab60e
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2025-10-14 16:00:47 -0400

    Update supernova_models.py

--- redback_jax/models/supernova_models.py
+++ redback_jax/models/supernova_models.py
@@ -2,6 +2,8 @@
 JAX-friendly classes for supernova modeling.
 """
 
+import unxt as uu
+
 from jax import jit
 import jax.numpy as jnp
 
@@ -63,7 +65,7 @@ def arnett_bolometric(
     f_nickel,
     mej,
     *,
-    interaction_process="diffusion",
+    apply_diffusion=True,
     vej=None,
     kappa=None,
     kappa_gamma=None,
@@ -73,17 +75,16 @@ def arnett_bolometric(
     :param time: time in days
     :param f_nickel: fraction of nickel mass
     :param mej: total ejecta mass in solar masses
-    :param kappa: opacity (required if interaction_process is not None)
-    :param kappa_gamma: gamma-ray opacity (required if interaction_process is not None)
-    :param vej: ejecta velocity in km/s (required if interaction_process is not None)
+    :param kappa: opacity (required if apply_diffusion is True)
+    :param kappa_gamma: gamma-ray opacity (required if apply_diffusion is True)
+    :param vej: ejecta velocity in km/s (required if apply_diffusion is True)
     :param dense_resolution: Number of points to use in the dense time array if 
-        interaction_process is not None
-    :param interaction_process: Name of the interaction process to apply (as a string).
-        Default is diffusion. Output can be None to return raw luminosity.
+        apply_diffusion is True
+    :param apply_diffusion: True if diffusion interaction process should be applied.
     :return: bolometric_luminosity in erg/s
     """
     lbol = _nickelcobalt_engine(time=time, f_nickel=f_nickel, mej=mej)
-    if interaction_process == "diffusion":
+    if apply_diffusion:
         dense_times = jnp.linspace(0, time[-1]+100, dense_resolution)
         dense_lbols = _nickelcobalt_engine(time=dense_times, f_nickel=f_nickel, mej=mej)
         _, new_luminosity = diffusion_convert_luminosity(
@@ -97,3 +98,86 @@ def arnett_bolometric(
         )
         lbol = new_luminosity
     return lbol
+
+# We compile the arnett_bolometric function with JIT, specifying static arguments.
+# Changing these will require recompilation.
+arnett_bolometric_jit = jit(arnett_bolometric, static_argnames=('apply_diffusion', 'dense_resolution'))
+
+
+@citation_wrapper('https://ui.adsabs.harvard.edu/abs/1982ApJ...253..785A/abstract')
+def arnett_model(
+    time,
+    f_nickel,
+    mej,
+    *,
+    redshift=0.0,
+    cosmo_H0=None,
+    cosmo_Om0=None,
+    apply_diffusion=True,
+    vej=None,
+    kappa=None,
+    kappa_gamma=None,
+    temperature_floor=None,
+    output_format="magnitude",
+):
+    """
+    A version of the arnett model where SED has time-evolving spectral features.
+
+    :param time: time in days
+    :param redshift: source redshift
+    :param f_nickel: fraction of nickel mass
+    :param mej: total ejecta mass in solar masses
+    :param redshift: source redshift
+    :param cosmo_H0: Hubble constant to use for luminosity distance calculation.
+    :param cosmo_Om0: Matter density to use for luminosity distance calculation.
+    :param apply_diffusion: True if diffusion interaction process should be applied.
+    :param kappa: opacity (required if apply_diffusion is True)
+    :param kappa_gamma: gamma-ray opacity (required if apply_diffusion is True)
+    :param vej: ejecta velocity in km/s (required if apply_diffusion is True)
+    :param temperature_floor: Floor temperature in kelvin (required if photosphere is temperature_floor)
+    :param output_format: 'magnitude', 'spectra', 'flux'
+    :param bands: Required if output_format is 'magnitude' or 'flux'.
+
+    :return: set by output format - 'magnitude', 'spectra', 'flux'
+    """
+    dl = wcosmo.luminosity_distance(redshift, cosmo_H0, cosmo_Om0)
+    
+    time_obs = time
+    lambda_observer_frame = jnp.geomspace(100, 60000, 100)
+    time_temp = jnp.geomspace(0.1, 3000, 3000)  # in days
+    time_observer_frame = time_temp * (1. + redshift)
+    frequency, time = calc_kcorrected_properties(
+        frequency=lambda_to_nu(lambda_observer_frame),
+        redshift=redshift,
+        time=time_observer_frame
+    )
+
+    lbol = arnett_bolometric_jit(
+        time=time,
+        f_nickel=f_nickel,
+        mej=mej,
+        apply_diffusion=apply_diffusion,
+        vej=vej,
+        kappa=kappa,
+        kappa_gamma=kappa_gamma,
+    )
+
+    # Use a temperature floor photosphere model.
+    photo_temp, r_photo = compute_temperature_floor(
+        time=time,
+        luminosity=lbol,
+        vej=vej,
+        temperature_floor=temperature_floor,
+    )
+
+    # Use the blackbody SED.
+    fmjy = blackbody_to_flux_density(
+        temperature=photo_temp,
+        r_photosphere=r_photo,
+        dl=dl,
+        frequency=frequency[:, None],
+    ).T
+
+    #equivalencies = uu.spectral_density(frequency * uu.Hz, wav=lambda_observer_frame * uu.Angstrom)
+    #spectra = fmjy.to(uu.mJy).to(uu.erg / uu.cm ** 2 / uu.s / uu.Angstrom, equivalencies=equivalencies)
+    #return spectra
