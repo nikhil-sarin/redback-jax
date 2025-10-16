073584e6a77339eff7654c79e9d634d8534c2d0c
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2025-10-16 11:44:00 -0400

    Make some of the values fixed

--- redback_jax/models/supernova_models.py
+++ redback_jax/models/supernova_models.py
@@ -16,6 +16,11 @@ from redback_jax.interaction_processes import diffusion_convert_luminosity
 from redback_jax.photosphere import compute_temperature_floor
 
 
+# Planck18 cosmology defaults
+PLANCK18_H0 = 67.66  # km/s/Mpc
+PLANCK18_OM0 = 0.3111
+
+
 def blackbody_to_flux_density(temperature, r_photosphere, dl, frequency):
     """
     A general blackbody_to_flux_density formula
@@ -60,77 +65,63 @@ def _nickelcobalt_engine(time, f_nickel, mej):
 
 
 @citation_wrapper('https://ui.adsabs.harvard.edu/abs/1982ApJ...253..785A/abstract')
+@jit
 def arnett_bolometric(
     time,
     f_nickel,
     mej,
     *,
-    interaction_process="diffusion",
     vej=None,
     kappa=None,
     kappa_gamma=None,
-    dense_resolution=1000,
 ):
     """
     Compute the bolometric luminosity using the Arnett model.
 
-    When JIT compiling, set interaction_process and dense_resolution as static arguments.
-
     :param time: time in days
     :param f_nickel: fraction of nickel mass
     :param mej: total ejecta mass in solar masses
-    :param kappa: opacity (required if interaction_process is "diffusion")
-    :param kappa_gamma: gamma-ray opacity (required if interaction_process is "diffusion")
-    :param vej: ejecta velocity in km/s (required if interaction_process is "diffusion")
-    :param dense_resolution: Number of points to use in the dense time array if
-        interaction_process is "diffusion"
-    :param interaction_process: Interaction process to apply. If None then no interaction
-        process is applied. Currently only "diffusion" is supported.
+    :param kappa: opacity (required)
+    :param kappa_gamma: gamma-ray opacity (required)
+    :param vej: ejecta velocity in km/s (required)
     :return: bolometric_luminosity in erg/s
     """
     lbol = _nickelcobalt_engine(time=time, f_nickel=f_nickel, mej=mej)
-    if interaction_process == "diffusion":
-        dense_times = jnp.linspace(0, time[-1]+100, dense_resolution)
-        dense_lbols = _nickelcobalt_engine(time=dense_times, f_nickel=f_nickel, mej=mej)
-        _, new_luminosity = diffusion_convert_luminosity(
-            time=time,
-            dense_times=dense_times,
-            luminosity=dense_lbols,
-            mej=mej,
-            kappa=kappa,
-            kappa_gamma=kappa_gamma,
-            vej=vej,
-        )
-        lbol = new_luminosity
-    else:
-        raise ValueError(f"Unsupported interaction_process: {interaction_process}")
-    return lbol
 
-# We compile the arnett_bolometric function with JIT, specifying static arguments.
-# Changing these will require recompilation.
-arnett_bolometric_jit = jit(arnett_bolometric, static_argnames=('interaction_process', 'dense_resolution'))
+    # Perform the diffusion interaction process.
+    dense_times = jnp.linspace(0, time[-1]+100, 1000)
+    dense_lbols = _nickelcobalt_engine(time=dense_times, f_nickel=f_nickel, mej=mej)
+    _, new_luminosity = diffusion_convert_luminosity(
+        time=time,
+        dense_times=dense_times,
+        luminosity=dense_lbols,
+        mej=mej,
+        kappa=kappa,
+        kappa_gamma=kappa_gamma,
+        vej=vej,
+    )
+    lbol = new_luminosity
+
+    return lbol
 
 
 @citation_wrapper('https://ui.adsabs.harvard.edu/abs/1982ApJ...253..785A/abstract')
+@jit
 def arnett_model(
     time,
     f_nickel,
     mej,
     *,
     redshift=0.0,
-    cosmo_H0=67.66,  # Planck 2018 value
-    cosmo_Om0=0.30966,
-    interaction_process="diffusion",
+    cosmo_H0=PLANCK18_H0,
+    cosmo_Om0=PLANCK18_OM0,
     vej=None,
     kappa=None,
     kappa_gamma=None,
     temperature_floor=None,
-    output_format="magnitude",
 ):
     """
     A version of the arnett model where SED has time-evolving spectral features.
-
-    When JIT compiling, set interaction_process and output_format as static arguments.
 
     :param time: time in days
     :param redshift: source redshift
@@ -139,15 +130,12 @@ def arnett_model(
     :param redshift: source redshift
     :param cosmo_H0: Hubble constant to use for luminosity distance calculation.
     :param cosmo_Om0: Matter density to use for luminosity distance calculation.
-    :param interaction_process: Interaction process to apply. If None then no interaction
-    :param kappa: opacity (required if interaction_process is "diffusion")
-    :param kappa_gamma: gamma-ray opacity (required if interaction_process is "diffusion")
-    :param vej: ejecta velocity in km/s (required if interaction_process is "diffusion")
+    :param kappa: opacity (required)
+    :param kappa_gamma: gamma-ray opacity (required)
+    :param vej: ejecta velocity in km/s (required)
     :param temperature_floor: Floor temperature in kelvin (required if photosphere is temperature_floor)
-    :param output_format: 'magnitude', 'spectra', 'flux'
-    :param bands: Required if output_format is 'magnitude' or 'flux'.
 
-    :return: set by output format - 'magnitude', 'spectra', 'flux'
+    :return: The spectra output.
     """
     # Wcosmo returns in Mpc (though it is marked as km/s), so we need to
     # correct the units and convert to cm.
@@ -166,7 +154,6 @@ def arnett_model(
         time=time,
         f_nickel=f_nickel,
         mej=mej,
-        interaction_process=interaction_process,
         vej=vej,
         kappa=kappa,
         kappa_gamma=kappa_gamma,
@@ -191,20 +178,8 @@ def arnett_model(
     # Convert erg/s/Hz/cm^2 to erg/cm^2/s/Angstrom using 2.998e18 for the
     # speed of light in Angstrom/s. We define this numerically to be JAX-friendly.
     spectra = spectral_flux_density * 2.998e18 / (lambda_observer_frame[None, :] ** 2)
-
-    if output_format == "spectra":
-        return namedtuple('output', ['time', 'lambdas', 'spectra'])(
-            time=time_observer_frame,
-            lambdas=lambda_observer_frame,
-            spectra=spectra
-        )
-    elif output_format == "magnitude":
-        raise NotImplementedError("Magnitude output not yet implemented.")
-    elif output_format == "flux":
-        raise NotImplementedError("Flux output not yet implemented.")
-    else:
-        raise ValueError(f"Unknown output_format: {output_format}")
-
-# JIT-compiled version of arnett_model with static arguments
-# Changing interaction_process or output_format will require recompilation
-arnett_model_jit = jit(arnett_model, static_argnames=('interaction_process', 'output_format'))
+    return namedtuple('output', ['time', 'lambdas', 'spectra'])(
+        time=time_observer_frame,
+        lambdas=lambda_observer_frame,
+        spectra=spectra
+    )
